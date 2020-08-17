5774ebd14e1b03a6ee72b779e3928fbefea06679
Andrew Reid <areid.sj@gmail.com>
2020-08-17 15:17:37 -0600

    Pass all tests

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,11 @@
 def convert(number):
-    pass
+    output = ""
+    if number % 3 == 0:
+        output = output + "Pling"
+    if number % 5 == 0:
+        output = output + "Plang"
+    if number % 7 == 0:
+        output = output + "Plong"
+    if output == "":
+        output = output + str(number)
+    return output
